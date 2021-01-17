68f46e536d2f71dc8cb47d73ce2c84a2a6d4e862
Berg-amoth <alexis.japas@protonmail.com>
2021-01-17 21:26:13 +0100

    STRUCT: OK. TODO: JSON.

--- main.c
+++ main.c
@@ -10,7 +10,6 @@ typedef struct Aero Aero;
 struct Vol
 {
     Aero* destination;
-    struct tm * date_depart;
     int annee_depart;
     int mois_depart;
     int jour_depart;
@@ -26,11 +25,20 @@ struct Vol
 struct Aero
 {
     char nom_aeroport[50];
+    char code[10];
+    char ville[50];
+    char pays[50];
+    int decalage_utc;
     Vol* liste_vols;
 };
 
 
 // TRADUCTION JSON
+// Recuperation des aeroports
+Aero* initialiser_aeroports() {
+    Aero* liste_aeroports;
+    return liste_aeroports;
+}
 
 
 // VÉRIFICATION DATES
@@ -42,6 +50,15 @@ struct Aero
 // MAIN
 int main(int argc, char const *argv[])
 {
+    // Liste des aeroports (uniquement ceux dont on connait les vols)
+    Aero* aeroports = initialiser_aeroports();
+
+
+
+
+
+
+    // TESTS
     Aero* aeroport1 = malloc(sizeof(*aeroport1));
     strcpy(aeroport1->nom_aeroport, "Charles de Gaulle");
 
@@ -54,9 +71,6 @@ int main(int argc, char const *argv[])
     vol1->mois_depart = 10;
     vol1->jour_depart = 12;
     vol1->heure_depart = 15;
-    
-
-    printf("%d %d\n", vol1->jour_depart, vol1->heure_depart);
     
     
     return 0;
